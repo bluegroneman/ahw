1b0d4619dcbf7ddf596eba9e2fdfaf899823bf5e
bluegroneman <bluegroneman10@gmail.com>
2025-12-29 18:25:14 -0700

    update for 2025 WCF grant

--- src/components/Quote.svelte
+++ src/components/Quote.svelte
@@ -2,13 +2,15 @@
     export let quote = ""
     export let author = ""
 </script>
-<div class="max-w-fit my-4 p-4 text-gray-800 rounded-lg shadow from-t-teal to-t-blue bg-gradient-to-l">
-    <div class="mb-2 justify-center">
-        <div class="h-3 text-3xl text-left text-white">“</div>
-        <p class="px-4 text-lg text-left text-white">
-            {quote}
-        </p>
-        <p class="font-bold mt-4 text-t-orange">- {author}</p>
-        <div class="h-3 text-3xl text-right text-t-white">”</div>
-    </div>
+<div class="max-w-fit my-6 p-4 text-gray-800 rounded-lg shadow">
+    <figure class="border-l border-indigo-600 pl-8 dark:border-indigo-400">
+      <blockquote class="text-xl/8 font-semibold tracking-tight text-gray-900">
+        <p>"{quote}"</p>
+      </blockquote>
+      <figcaption class="mt-8 flex gap-x-2">
+        <div class="text-sm/6">
+          <div class="font-semibold text-gray-900">{author}</div>
+        </div>
+      </figcaption>
+    </figure>
 </div>
